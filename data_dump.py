c8ff372047de93fba923df6eced26ba906d1e681
vinodkumargr <vks7483437@gmail.com>
2023-04-07 17:05:33 +0530

    data inserted into mongodb

---
+++ data_dump.py
@@ -0,0 +1,26 @@
+import pymongo
+import pandas as pd
+import json
+
+
+
+client = pymongo.MongoClient("mongodb+srv://vinod:insurance_prediction@cluster1.ixse1h4.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+
+DATA_FILE_PATH = "C://Insurance_project//Insurance_prediction//insurance.csv"
+DATABASE_NAME = "INSURANCE"
+COLLECTION_NAME = "INSURANCE_PROJECT"
+
+if __name__ == "__main__":
+    data = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns : {data.shape}")
+    
+    
+    data.drop(columns="index", inplace=True)
+    
+    
+    json_record = list(json.loads(data.T.to_json()).values())
+    print(json_record[0])
+    
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
