080b8acfebcb5c7797ab8fd9d92d9712464cbfc7
vinodkumargr <vks7483437@gmail.com>
2023-04-11 13:31:01 +0530

    utils and config file updated

--- insurance_pred/config.py
+++ insurance_pred/config.py
@@ -5,12 +5,7 @@ import json
 import os, sys
 from dataclasses import dataclass
 
-@dataclass
-class EnvironmentVariable:
-    mongo_db_url:str = os.getenv("MONGO_DB_URL")
-    
-    
-env_var = EnvironmentVariable()
-mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
+
+mongo_client = pymongo.MongoClient(os.getenv("MONGO_DB_URL"))
 TARGET_COLUMN = "charges"
-print(f"mongo client: {env_var.mongo_db_url}")
+print(f"mongo client: {mongo_client}")
